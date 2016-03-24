a8761aa5794ef4bd8b08effe2455829db12d4260
Mei-Hui Su <mei@Mei-Huis-MacBook-Air.local>
2016-03-23 18:44:17 -0700

    onload is with window not with $window

--- js/viewer.js
+++ js/viewer.js
@@ -1501,7 +1501,7 @@ $('.y-list-select2').select2({theme:"classic"});
    }
 })
 
-$(window).onload(function() {
+window.onload(function() {
    if(enableEmbedded) {
      window.console.log("reset2InitPlot got called from window.load");
      reset2InitPlot();
